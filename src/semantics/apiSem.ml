3d11911faa5145b7af5e38ef8e0ae6048b9dfec7
OH Gyuhyeok <retato@snu.ac.kr>
2026-06-06 13:25:48 +0900

    apiSem: model ferror_unlocked

--- src/semantics/apiSem.ml
+++ src/semantics/apiSem.ml
@@ -224,6 +224,7 @@ ApiMap.empty
 |> ApiMap.add "fputs_unlocked" {arg_typs = [Skip; Skip]; ret_typ = ones}
 |> ApiMap.add "putchar_unlocked" {arg_typs = [Skip]; ret_typ = ones}
 |> ApiMap.add "putc_unlocked" {arg_typs = [Skip; Skip]; ret_typ = ones}
+|> ApiMap.add "ferror_unlocked" {arg_typs = [Skip]; ret_typ = int_v}
 |> ApiMap.add "fflush_unlocked" {arg_typs = [Skip]; ret_typ = int_v}
 |> ApiMap.add "puts" {arg_typs = [Skip]; ret_typ = ones}
 |> ApiMap.add "recv" {arg_typs = [Skip; buf_q; Size; Skip]; ret_typ = SizeArg}
