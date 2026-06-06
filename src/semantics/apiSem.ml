21d0279fd0b29f88a4e58b3d2713e16421821f15
OH Gyuhyeok <retato@snu.ac.kr>
2026-06-06 13:12:54 +0900

    apiSem: model fflush_unlocked

--- src/semantics/apiSem.ml
+++ src/semantics/apiSem.ml
@@ -224,6 +224,7 @@ ApiMap.empty
 |> ApiMap.add "fputs_unlocked" {arg_typs = [Skip; Skip]; ret_typ = ones}
 |> ApiMap.add "putchar_unlocked" {arg_typs = [Skip]; ret_typ = ones}
 |> ApiMap.add "putc_unlocked" {arg_typs = [Skip; Skip]; ret_typ = ones}
+|> ApiMap.add "fflush_unlocked" {arg_typs = [Skip]; ret_typ = int_v}
 |> ApiMap.add "puts" {arg_typs = [Skip]; ret_typ = ones}
 |> ApiMap.add "recv" {arg_typs = [Skip; buf_q; Size; Skip]; ret_typ = SizeArg}
 |> ApiMap.add "send" {arg_typs = [Skip; arr_src_q; Size; Skip]; ret_typ = SizeArg}
