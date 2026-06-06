cfc41bcf161e454b4fd8d76092a89cc7e5018227
OH Gyuhyeok <retato@snu.ac.kr>
2026-06-06 10:19:58 +0900

    apiSem: model printf

--- src/semantics/apiSem.ml
+++ src/semantics/apiSem.ml
@@ -219,6 +219,7 @@ ApiMap.empty
 |> ApiMap.add "write" {arg_typs = [Skip; arr_src_q; Size]; ret_typ = SizeArg}
 |> ApiMap.add "fwrite" {arg_typs = [arr_src_q; Skip; Size; Skip]; ret_typ = SizeArg}
 |> ApiMap.add "fprintf" {arg_typs = [Skip; Skip]; ret_typ = int_v}
+|> ApiMap.add "printf" {arg_typs = [Skip]; ret_typ = int_v}
 |> ApiMap.add "fputs_unlocked" {arg_typs = [Skip; Skip]; ret_typ = ones}
 |> ApiMap.add "putchar_unlocked" {arg_typs = [Skip]; ret_typ = ones}
 |> ApiMap.add "puts" {arg_typs = [Skip]; ret_typ = ones}
