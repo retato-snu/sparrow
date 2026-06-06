a57fdaa5d816510727998f972ad5845021bba519
OH Gyuhyeok <retato@snu.ac.kr>
2026-06-06 09:18:55 +0900

    apiSem: model puts

--- src/semantics/apiSem.ml
+++ src/semantics/apiSem.ml
@@ -219,6 +219,7 @@ ApiMap.empty
 |> ApiMap.add "fwrite" {arg_typs = [arr_src_q; Skip; Size; Skip]; ret_typ = SizeArg}
 |> ApiMap.add "fputs_unlocked" {arg_typs = [Skip; Skip]; ret_typ = ones}
 |> ApiMap.add "putchar_unlocked" {arg_typs = [Skip]; ret_typ = ones}
+|> ApiMap.add "puts" {arg_typs = [Skip]; ret_typ = ones}
 |> ApiMap.add "recv" {arg_typs = [Skip; buf_q; Size; Skip]; ret_typ = SizeArg}
 |> ApiMap.add "send" {arg_typs = [Skip; arr_src_q; Size; Skip]; ret_typ = SizeArg}
 |> ApiMap.add "nl_langinfo" {arg_typs = [Skip]; ret_typ = int_v}
