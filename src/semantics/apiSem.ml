3895ae8e1043dfefc22989407701e972c8c71fff
OH Gyuhyeok <retato@snu.ac.kr>
2026-06-07 12:10:35 +0900

    Model user lookup boundaries

--- src/semantics/apiSem.ml
+++ src/semantics/apiSem.ml
@@ -180,6 +180,8 @@ ApiMap.empty
 |> ApiMap.add "setegid" {arg_typs = [Skip]; ret_typ = ones}
 |> ApiMap.add "getgid" {arg_typs = []; ret_typ = int_v}
 |> ApiMap.add "getegid" {arg_typs = []; ret_typ = int_v}
+|> ApiMap.add "geteuid" {arg_typs = []; ret_typ = int_v}
+|> ApiMap.add "getpwuid" {arg_typs = [Skip]; ret_typ = AllocStruct}
 |> ApiMap.add "htonl" {arg_typs = [v_src]; ret_typ = TopWithSrcTaint}
 |> ApiMap.add "htons" {arg_typs = [v_src]; ret_typ = TopWithSrcTaint}
 |> ApiMap.add "ntohl" {arg_typs = [v_src]; ret_typ = TopWithSrcTaint}
